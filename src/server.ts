03f9868fc3847f5c602552df5d8cab1fdd5f1ac7
ANASTASIIA <anastasiaposokhova@gmail.com>
2025-10-07 11:16:05 +0200

    upd server

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,13 @@ import Notification from "./db/Notification";
 const startServer = () => {
   const app = express();
 
-  app.use(cors({ origin: process.env.FRONTEND_URL || "*" }));
+  app.use(
+    cors({
+      origin: process.env.FRONTEND_URL,
+      credentials: true,
+    })
+  );
+
   app.use(express.json());
   app.use(
     "/uploads",
